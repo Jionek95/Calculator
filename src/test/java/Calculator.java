69e2642280d6047cb4ee787bbee9cbdf27aed1d8
Jionek95 <jakopiczko@wp.pl>
2023-11-01 23:33:32 +0100

    Add \n to regex

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 public class Calculator {
     public static int add(String givenString) {
         if (!givenString.isEmpty()) {
-            List<Integer> numbers = Stream.of(givenString.split(","))
+            List<Integer> numbers = Stream.of(givenString.split(",|\n"))
                     .map(Integer::parseInt)
                     .toList();
             return numbers.stream()
