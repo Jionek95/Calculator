6d762b04377c4d0743b331af6e0aeceab022ed87
Jionek95 <jakopiczko@wp.pl>
2023-11-01 13:01:56 +0100

    Update add() method - split givenString to comma separated elements - then count sum of all Integer elements of the stream

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -1,7 +1,15 @@
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Calculator {
-    public static int add(String s) {
-        if (!s.isEmpty()) {
-            return Integer.parseInt(s);
+    public static int add(String givenString) {
+        if (!givenString.isEmpty()) {
+            List<Integer> numbers = Stream.of(givenString.split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+            return numbers.stream()
+                    .reduce(Integer::sum)
+                    .orElseThrow();
         }
         return 0;
     }
