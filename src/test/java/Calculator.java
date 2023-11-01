e996d710518f02f68d78214077c557e9d2b49ed5
Jionek95 <jakopiczko@wp.pl>
2023-11-01 23:38:56 +0100

    Extract strArrayToIntList()

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -4,14 +4,18 @@ import java.util.stream.Stream;
 public class Calculator {
     public static int add(String givenString) {
         if (!givenString.isEmpty()) {
-            List<Integer> numbers = Stream.of(getSplit(givenString))
-                    .map(Integer::parseInt)
-                    .toList();
+            List<Integer> numbers = strArrayToIntList(givenString);
             return numbers.stream()
                     .reduce(Integer::sum)
                     .orElseThrow();
         }
         return 0;
+    }
+
+    private static List<Integer> strArrayToIntList(String givenString) {
+        return Stream.of(getSplit(givenString))
+                .map(Integer::parseInt)
+                .toList();
     }
 
     private static String[] getSplit(String givenString) {
