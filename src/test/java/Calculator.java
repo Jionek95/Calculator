f73584c2587d61f5851a6c107f7dd7dc1b57e0b3
Jionek95 <jakopiczko@wp.pl>
2023-11-02 09:30:44 +0100

    Create method for handling negatives

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -1,15 +1,28 @@
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Calculator {
     public static int add(String givenString) {
         if (!givenString.isEmpty()) {
             List<Integer> numbers = strArrayToIntList(getSplit(givenString));
+            negatives(numbers);
             return numbers.stream()
                     .reduce(Integer::sum)
                     .orElseThrow();
         }
         return 0;
+    }
+
+    private static void negatives(List<Integer> numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        numbers.stream()
+                .filter(num->num<0)
+                .forEach(num->stringBuilder.append(num).append(" "));
+        if (!stringBuilder.toString().isEmpty()){
+            throw new RuntimeException("negatives are not allowed " + stringBuilder.toString());
+        }
     }
 
     private static List<Integer> strArrayToIntList(String[] strArray) {
